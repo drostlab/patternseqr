fd832dd0797d574453d04bfa6a88e7e4d4c0c861
HajkD <hajkdrost@googlemail.com>
2020-04-22 12:23:05 +0200

    add package backbone

---
+++ R/detect_seq_pattern.R
@@ -0,0 +1,5 @@
+#' @title Main function to determin sequence features
+#' @export
+detect_seq_pattern <- function () {
+
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(patternseqr)
+
+test_check("patternseqr")
